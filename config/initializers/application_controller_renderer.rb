246f108ab7f9cd2d48f08fbabbdfaef698593137
vagrant <vagrant@localhost.localdomain>
2020-05-30 07:58:35 +0000

    Fix ログインを名前でできるようにする

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '327c0b63d16fa536f5c85b75425b963359beba6fcc342f51a79aa29c25aef3854322a08c322a47255698cd3d3898bc94652eecb24fd512cbc16ef9c935f9ab98'
